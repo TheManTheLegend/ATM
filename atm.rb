c8d7111b2fa68cd7dc71ad3451e3cd9b5bd8df4a
Nik Young <youngnr@s.dcsdk12.org>
2015-11-06 10:22:38 -0700

    I did it

--- atm.rb
+++ atm.rb
@@ -4,41 +4,10 @@ class Atm
 		@pin = pin
 		@person = "nan"
 	end
-	def login
-		puts "Please input you PIN"
-		pin = gets.to_f
-		if pin == @pin 
-			@logged = true
-			puts "Welcome #{@name}"
-		else
-			puts "I'm sorry that PIN does not match any users in our database, please try again."
-		end
-		if @logged == true
-			withdraw
-		end
-	end
-	if @pin == 1771
-		@person = User.new("John", 1771, 1250)
-	elsif @pin == 0220
-		@person = User.new("Sarah", 0220, 9000)
-	elsif @pin == 9999
-		@person = User.new("Karl", 9999, 7)
-	else
-		puts "I'm sorry that PIN does not match any users in our database, please try again."
-	end
+	
+
+
 	
 	
-	def withdraw
-		puts "How much would you like to withdraw?"
-		with = gets.to_f
-		if @balance >= with
-			@balance = @balance.to_f - with
-			puts "Thank you for your withdraw of $#{with}."
-			puts "You have $#{@balance} left in your account."
-			puts "Have a nice day, #{@name}!"
-		else
-			puts "Sorry #{@name}, you don't have that much in your account."
-		end
-		@logged = false
-	end
+	
 end
--- run.rb
+++ run.rb
@@ -2,8 +2,29 @@ require_relative "atm"
 require_relative "user"
 require "gosu"
 
-John = User.new("John", 1771, 1250)
-Sarah = User.new("Sarah", 0220, 9000)
-Karl = User.new("Karl", 9999, 7)
-John.login
+
+def login
+		puts "Please input you PIN"
+		pin = gets.to_i
+		if pin == 1771
+			@person = User.new("John", pin, 1250)
+			@logged = true
+		elsif pin == 1221
+			@person = User.new("Sarah", pin, 9000)
+			@logged = true
+		elsif pin == 9999
+			@person = User.new("Karl", pin, 7)
+			@logged = true
+		elsif pin == 1499
+			@person = User.new("Mr.Padjen", pin, 1000000)
+			@logged = true
+		else
+			puts "I'm sorry that PIN does not match any users in our database, please try again."
+		end
+		if @logged == true
+				@person.ask
+		end
+end
+
+login
 
